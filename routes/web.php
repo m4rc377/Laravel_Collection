f0c43337f64cc32341c5546f20e0d0741736e539
wael salah <wael.fci@gmail.com>
2019-05-16 01:11:46 +0200

    preparing login page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home.index');
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+
+    Route::get('/', function () {
+        return view('pages.home.index');
+    });
 });
+
+Auth::routes();
